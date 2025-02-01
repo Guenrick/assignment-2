33cf5e4dfcd7c9763803558b5fc946003b8c0a7f
Guenrick <147289316+Guenrick@users.noreply.github.com>
2025-02-01 09:22:58 -0300

    Primeiro commit da atv Valor.hs

--- src/Valor.hs
+++ src/Valor.hs
@@ -1,5 +1,29 @@
-module Valor where
+module Vendas where
 
-valorTotal :: Double -> Double -> Double
-valorTotal morangos maçãs
-  = undefined
+import System.IO (stdout, hSetBuffering, BufferMode(NoBuffering))
+valorTotal morangos maçãs = tmorangos + tmaças
+
+  where
+    tmorangos
+      | morangos > 5 = morangos*7.2
+      | morangos <= 5 = morangos*8.5
+
+    tmaças
+      | maçãs < 5 = maçãs * 5.25
+      | maçãs >= 5 = maçãs * 4.75
+
+
+
+main :: IO ()
+main = do
+  putStrLn"Digite a quantidade de Morangos (em kg): "
+  morangos <- readLn
+  putStrLn"Digite a quantidade de Maçãs (em kg): "
+  maçãs <- readLn
+
+  let valor = valorTotal morangos maçãs
+  if valor < 0
+    then
+    putStrLn"Entrada inválida"
+    else putStrLn("O valor total da compra é R$ " ++ show(valor))
+
