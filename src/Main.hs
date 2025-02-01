f0494d65f1a519486fdf3407ce17d2c39beb6c04
Guenrick <147289316+Guenrick@users.noreply.github.com>
2025-02-01 09:34:12 -0300

    Update Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,29 @@
-module Main where
-
-import Valor (valorTotal)
+module Valor where
 
 import System.IO (stdout, hSetBuffering, BufferMode(NoBuffering))
+valorTotal morangos maçãs = tmorangos + tmaças
+
+  where
+    tmorangos
+      | morangos > 5 = morangos*7.2
+      | morangos <= 5 = morangos*8.5
+
+    tmaças
+      | maçãs < 5 = maçãs * 5.25
+      | maçãs >= 5 = maçãs * 4.75
+
+
 
 main :: IO ()
-main =
-  undefined
+main = do
+  putStrLn"Digite a quantidade de Morangos (em kg): "
+  morangos <- readLn
+  putStrLn"Digite a quantidade de Maçãs (em kg): "
+  maçãs <- readLn
+
+  let valor = valorTotal morangos maçãs
+  if valor < 0
+    then
+    putStrLn"Entrada inválida"
+    else putStrLn("O valor total da compra é R$ " ++ show(valor))
+
